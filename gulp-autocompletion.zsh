fa6c0238052aefe2842ad446f9432ce85774d480
Mikael Brevik <mikaelbre@gmail.com>
2014-05-30 23:40:19 +0200

    Changes to Gulps own implementation of code completion.

--- gulp-autocompletion.zsh
+++ gulp-autocompletion.zsh
@@ -2,12 +2,12 @@
 
 #
 # gulp-autocompletion-zsh
-# 
+#
 # Autocompletion for your gulp.js tasks
 #
 # Copyright(c) 2014 André König <andre.koenig@posteo.de>
 # MIT Licensed
-# 
+#
 
 #
 # André König
@@ -15,15 +15,6 @@
 # Twitter: https://twitter.com/caiifr
 #
 
-#
-# Grabs all available tasks from the `gulpfile.js`
-# in the current directory.
-#
-function $$gulp_completion() {
-    compls=$(grep -Eo "gulp.task\(('(([a-zA-Z0-9]|-)*)',)" gulpfile.js 2>/dev/null | grep -Eo "'(([a-zA-Z0-9]|-)*)'" | sed s/"'"//g | sort)
-
-    completions=(${=compls})
-    compadd -- $completions
-}
-
-compdef $$gulp_completion gulp
+# Uses gulps completion. See
+# https://github.com/gulpjs/gulp/tree/master/completion
+eval "$(gulp --completion=zsh 2>/dev/null)"
